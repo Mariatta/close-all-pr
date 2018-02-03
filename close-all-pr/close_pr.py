47cb8f984c73f942dc233e38ea495d1f75ff6954
Mariatta Wijaya <mariatta@python.org>
2018-02-03 18:40:21 -0500

    Close all incoming pr

--- close-all-pr/close_pr.py
+++ close-all-pr/close_pr.py
@@ -9,5 +9,12 @@ router = gidgethub.routing.Router()
 
 @router.register("pull_request", action="opened")
 async def close_pr(event, gh, *args, **kwargs):
-    print("hello")
-    print(event)
+    data = {'state': 'closed',
+            'maintainer_can_modify': True}
+    await gh.patch(event.data["pull_request"]["url"], data=data)
+    pr_comment = {'body': f"Thanks for the PR @{event.data['user']['login']}. "
+                          f"But this repo is not accepting any PRs for now. "
+                          f"Closing your PR. "
+                          f"Signed, Mariatta's personal GitHub bot 🤖"
+                  }
+    await gh.post(event.data["pull_request"]["comments_url"], data=pr_comment)
